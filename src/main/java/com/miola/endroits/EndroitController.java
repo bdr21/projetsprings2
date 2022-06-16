94f2ed09531aa6e21518a55d8944086984bdf7ff
RAZZOUKI Chaimae <chaimaerazzouki.2017@gmail.com>
2022-06-16 23:02:37 +0100

    crud endroit + recherche

--- src/main/java/com/miola/endroits/EndroitController.java
+++ src/main/java/com/miola/endroits/EndroitController.java
@@ -1,22 +1,18 @@
 package com.miola.endroits;
 
 
-import com.miola.dto.ResponseWithArray;
+import com.miola.dto.ResponseWithRecordCount;
 import com.miola.messages.ControllerMessages;
+import com.miola.users.UserModel;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.BindingResult;
-import org.springframework.validation.FieldError;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
 
-import javax.validation.Valid;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
+import java.util.Optional;
 
 @RestController
 @RequestMapping(path = "/endroits")
@@ -33,14 +29,22 @@ public class EndroitController {
         return dataList;
     }
 
-    @GetMapping(path = "")
-    @ResponseStatus(code = HttpStatus.OK)
-    public ResponseEntity<ResponseWithArray> getAll() {
+//    @GetMapping(path = "")
+//    @ResponseStatus(code = HttpStatus.OK)
+//    public ResponseEntity<ResponseWithArray> getAll() {
+//        List<EndroitModel> list = endroitService.getAll();
+//        ResponseWithArray response = new ResponseWithArray(HttpStatus.OK, ControllerMessages.SUCCESS, list);
+//        return new ResponseEntity<ResponseWithArray>(response, response.getStatus());
+//    }
+
+    @GetMapping
+    public ResponseEntity<ResponseWithRecordCount> getAllEndroits(){
         List<EndroitModel> list = endroitService.getAll();
-        ResponseWithArray response = new ResponseWithArray(HttpStatus.OK, ControllerMessages.SUCCESS, list);
-        return new ResponseEntity<ResponseWithArray>(response, response.getStatus());
+        ResponseWithRecordCount response = new ResponseWithRecordCount(HttpStatus.OK, ControllerMessages.SUCCESS, list.size(), list);
+        return new ResponseEntity<>(response, response.getStatus());
     }
 
+    // A voir
     @GetMapping(path = "/{id}")
     public ResponseEntity<EndroitModel> getOne(@PathVariable int id) {
         List<EndroitModel> le = populateList();
@@ -51,12 +55,68 @@ public class EndroitController {
         return new ResponseEntity<>(endroit, HttpStatus.OK);
     }
 
-    @PostMapping(path = "")
-    public ResponseEntity<Object> addOne(@Validated @RequestBody EndroitModel endroit) {
+    // search endroits by ville
+    @GetMapping(path = "/ville/{ville}")
+    public ResponseEntity<List<EndroitModel>> getEndroitsByVille(@PathVariable String ville) {
+        // get all endroits from DB
+        List<EndroitModel> endroitsList = endroitService.getAll();
+        // declare the list that will contain the places of the specified city
         List<EndroitModel> le = new ArrayList<>();
-        le.add(endroit);
+        // put all the places which have the specified city in the list
+        for (EndroitModel e : endroitsList) {
+            if (e.getVille().equals(ville)) le. add(e);
+        }
+        if(! le.isEmpty())  return new ResponseEntity<>(le, HttpStatus.OK);
+        return new ResponseEntity<>(null, HttpStatus.NO_CONTENT);
+    }
+
+    // search endroit by name
+    @GetMapping(path = "/name/{name}")
+    public ResponseEntity<EndroitModel> getEndroitByName(@PathVariable String name) {
+        // get all endroits from DB
+        List<EndroitModel> endroitsList = endroitService.getAll();
+        for (EndroitModel e : endroitsList) {
+            if (e.getName().equals(name))
+                return new ResponseEntity<>(e, HttpStatus.OK);
+        }
+        return new ResponseEntity<>(null, HttpStatus.NO_CONTENT);
+    }
+
+    @PostMapping(path = "")
+    public ResponseEntity<Object> addEndroit(@Validated @RequestBody EndroitModel endroit) {
+        endroitService.addOne(endroit);
         return new ResponseEntity<>(endroit, HttpStatus.CREATED);
     }
+
+    @RequestMapping(value = "/update", method = RequestMethod.POST)
+    public ResponseEntity<Object> updateEndroit(@Validated @RequestBody EndroitModel endroit){
+        Optional<EndroitModel> endroitModel = endroitService.getOneById(endroit.getId());
+        if(endroitModel.isPresent()){
+            EndroitModel newEndroit = endroitModel.get();
+            newEndroit.setName(endroit.getName());
+            newEndroit.setVille(endroit.getVille());
+            endroitService.update(newEndroit);
+            return new ResponseEntity<>(newEndroit, HttpStatus.OK);
+        } else
+            return new ResponseEntity<>(null, HttpStatus.NO_CONTENT);
+    }
+
+    @GetMapping("/delete/{id}")
+    public HttpStatus delete(@PathVariable int id){
+        Optional<EndroitModel> endroitModel = endroitService.getOneById(id);
+        if(endroitModel.isPresent()){
+            endroitService.delete(id);
+            return HttpStatus.OK;
+        } else
+            return HttpStatus.BAD_REQUEST;
+    }
+
+//    @PostMapping(path = "")
+//    public ResponseEntity<Object> addOne(@Validated @RequestBody EndroitModel endroit) {
+//        List<EndroitModel> le = new ArrayList<>();
+//        le.add(endroit);
+//        return new ResponseEntity<>(endroit, HttpStatus.CREATED);
+//    }
 
 //    @ResponseStatus(HttpStatus.BAD_REQUEST)
 //    @ExceptionHandler(MethodArgumentNotValidException.class)
--- src/main/java/com/miola/endroits/EndroitService.java
+++ src/main/java/com/miola/endroits/EndroitService.java
@@ -1,10 +1,10 @@
 package com.miola.endroits;
 
-import com.miola.users.UserModel;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class EndroitService {
@@ -12,7 +12,15 @@ public class EndroitService {
     @Autowired
     private EndroitRepository endroitRepository;
 
+    public Optional<EndroitModel> getOneById(int id){ return endroitRepository.findById(id); }
+
     public List<EndroitModel> getAll() {
         return endroitRepository.findAll();
     }
+
+    public void addOne(EndroitModel endroitModel){ endroitRepository.save(endroitModel);}
+
+    public void update(EndroitModel endroitModel){ endroitRepository.save(endroitModel); }
+
+    public void delete(int id){ endroitRepository.deleteById(id); }
 }
