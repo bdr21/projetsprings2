3e11ed0da0b4848dfc953d099bb110a705baf982
oumaimayz28 <o.elyazyd@gmail.com>
2022-06-17 19:15:40 +0100

    new commit 2

--- src/main/java/com/miola/config/SecurityConfig.java
+++ src/main/java/com/miola/config/SecurityConfig.java
@@ -19,6 +19,7 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 
 import java.util.Arrays;
 
--- src/main/java/com/miola/endroit_crud/Repo/EndroitRepository.java
+++
@@ -1,8 +0,0 @@
-package com.miola.endroit_crud.Repo;
-
-
-import com.miola.endroit_crud.Models.Endroit;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EndroitRepository extends JpaRepository<Endroit, Long> {
-}
